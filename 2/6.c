c593160592d10471a291b765d200f134cfb917af
papablo <pabloatm980@gmail.com>
2017-04-05 19:21:52 -0300

    ejercicio 6 terminado, listo el pollo

---
+++ 2/6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <syscall.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_ALARM 5
+#define clear() printf("\033[H\033[J")
+
+
+void catch_alarm(int signum){
+
+	printf("\n*** TIMEOUT AMIGOL ***\n");
+	exit(1);
+
+}
+void catch_sigint(int signum){
+
+	signal(SIGINT,catch_sigint);
+	printf("\nSIGINT RECIBIDO!\n");
+	alarm(0);
+	alarm(MAX_ALARM);
+}
+
+
+int main (int agc, char const argv[]){
+
+	signal(SIGALRM,catch_alarm);
+	signal(SIGINT,catch_sigint);
+	alarm(MAX_ALARM);
+	printf("ARRANCAMO\n");
+	while(1){
+		
+	}
+
+	return 0;
+
+}
