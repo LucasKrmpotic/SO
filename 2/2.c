fceb7b56f931b092bc0956783331a140b734b1bb
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-04-08 00:57:14 -0300

    Comentarios agregados

--- 2/2.c
+++ 2/2.c
@@ -10,12 +10,21 @@
 #define clear() printf("\033[H\033[J")
 #define CMD "/bin/bash"
 
+/************************************
+ * Ejercicio implementado por:
+ *      KRMPOTIC, Lucas
+ *      SERRUYA ALOISI, Luciano
+ *      TOLEDO MARGALEF, Pablo
+ *
+ * *********************************/
+
 void catch_alarm(int signum){
+	/*rutina de atención para la señal SIGALRM*/
 	printf("\n*** TIMEOUT ***\n");
 	exit(1);
 }
 int main (int agc, char const argv[]){
-
+	//establecemos que la señal "SIGALRM" sea atendida con la función "catch_alarm"
 	signal(SIGALRM,catch_alarm);
 	
 	char *input;
@@ -24,22 +33,22 @@ int main (int agc, char const argv[]){
 		clear();
 
 		printf("login: ");
-
 		scanf(" %m[^\n]", &input);
-
 		printf("password: ");
 			
+        /* una vez solicitada la contraseña,
+         * empieza a correr el reloj para activar la alarma*/
 		alarm(MAX_ALARM);
 
 		scanf(" %m[^\n]", &pass);
-		
+		/*Si se ingresa la contraseña antes de que se acabe el tiempo
+         * se desactiva la alarma*/
 		alarm(0);
 		int status;
 		if(fork()){
 			//padre
 			waitpid(-1,&status,0);
 			clear();
-
 		}
 		else {
 			//hijo
--- 2/6.c
+++ 2/6.c
@@ -6,32 +6,42 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define MAX_ALARM 5
+#define MAX_ALARM 60
 #define clear() printf("\033[H\033[J")
 
+/************************************
+ * Ejercicio implementado por:
+ *      KRMPOTIC, Lucas
+ *      SERRUYA ALOISI, Luciano
+ *      TOLEDO MARGALEF, Pablo
+ *
+ * *********************************/
 
 void catch_alarm(int signum){
-
+	/*rutina de atención para la señal SIGALRM*/
 	printf("\n*** TIMEOUT ***\n");
 	exit(1);
 }
 void catch_sigint(int signum){
-
-	//signal(SIGINT,catch_sigint);
+	/*rutina de atención para la señal SIGINT*/
 	printf("\nSIGINT RECIBIDO!\n");
+	/*una vez recibida la señal, se desactiva la alarma pendiente
+	y se la reestablece al tiempo inicial*/
 	alarm(0);
 	alarm(MAX_ALARM);
 }
 
 
 int main (int agc, char const argv[]){
-
+	/*se configuran ambas señales y empieza a correr el tiempo
+	programa principal se queda esperando*/
 	signal(SIGALRM,catch_alarm);
 	signal(SIGINT,catch_sigint);
 	alarm(MAX_ALARM);
 	printf("---COMIENZO---\n");
+	
 	while(1);
-
+	
 	return 0;
 
 }
